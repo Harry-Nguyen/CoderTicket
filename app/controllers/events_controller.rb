0d8cd0f6b4c61538618da266e03181da99912cdb
Harry Nguyen <hamxvela@gmail.com>
2016-10-28 17:21:23 +0700

    Users can create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,4 +16,28 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    params[:event][:user_id] = session[:user_id]
+    params[:event][:starts_at] = DateTime.strptime(params[:event][:starts_at], '%d-%m-%Y %I:%M:%S %p')
+    params[:event][:ends_at] = DateTime.strptime(params[:event][:ends_at], '%d-%m-%Y %I:%M:%S %p')
+    params[:event][:published_at] = nil
+
+    @event = Event.new event_params
+    if @event.save
+      flash[:success] = 'Event is created successfully'
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:starts_at, :ends_at, :venue_id, :hero_image_url, :extended_html_description, :category_id, :name, :published_at, :user_id);
+  end
 end
